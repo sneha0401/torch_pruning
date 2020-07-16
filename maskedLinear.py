68c84148e7f3fbae1b0256845020369a74a3d0d3
Sneha Khandelwal <sneha0401>
2020-07-16 19:39:35 +0530

    corrected identation

--- maskedLinear.py
+++ maskedLinear.py
@@ -3,17 +3,15 @@ import torch.nn as nn
 from torch.autograd import Variable, Function
 import random
 #
+def randomarraygenerator(sparsity, self.masks):
+	seed(1)
+	max = int(sum(mask.view(-1).size(0) for mask in self.masks))
+	num_sparsed_els = (sparsity/100)*max
+	return random.sample(range(0, (max-1)), num_sparsed_els)
+
 class LinearFunction(Function):
 	def forward():
 
-def randomarraygenerator(sparsity):
-	seed(1)
-	max = numel() - 1
-	num_sparsed_els = (sparsity/100)*max
-	return random.sample(range(0, max), num_sparsed_els)
-
-def numel(self):
-		return int(sum(mask.view(-1).size(0) for mask in self.masks))
 
 class SparseLinear(nn.Module):
 	def __init__(self, num_inputs, num_outputs, sparsity):
@@ -24,12 +22,15 @@ class SparseLinear(nn.Module):
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		
 		self.weights = nn.Parameter()
-		
+
 		masks = torch.zeros_like(weights.view(-1))
 		zero_indicies = randomarraygenerator(sparsity)
 		for zi in zero_indicies:
 			m[zi] = 1
 		self.masks = torch.reshape(masks, weights.shape)
+
+	def numel(self):
+		return int(sum(mask.view(-1).size(0) for mask in self.masks))
 
 
 		
