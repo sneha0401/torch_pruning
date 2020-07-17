ef6f9c0c1abedee3095c24ccbaa6938758b99852
Sneha Khandelwal <sneha0401>
2020-07-17 21:44:21 +0530

    added xavier normal initializer and masks as parameters

--- maskedLinear.py
+++ maskedLinear.py
@@ -8,7 +8,12 @@ def randomarraygenerator(sparsity, mask_els):
 	return random.sample(range(0, (mask_els-1)), num_sparsed_els)
 '''
 class LinearFunction(Function):
-	def forward():
+	def __init__ (self, self.masks):
+		super(LinearFunction, self).__init__()
+		self.masks = self.masks
+	def forward(ctx, self, input, weight):
+		ctx.save_for_backward(input, weight)
+		output = input.mm(weight.mul_(self.mask.data).T)
 '''
 
 class SparseLinear(nn.Module):
@@ -19,14 +24,19 @@ class SparseLinear(nn.Module):
 		self.sparsity = sparsity
 		
 		device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-		self.weights = nn.Parameter(torch.Tensor(num_outputs, num_inputs))
+
+		weights = torch.Tensor(num_outputs, num_inputs)
+		wetghts =  nn.init.xavier_normal(weights)
+		self.weights = nn.Parameter(weights)
 
 		masks = torch.zeros_like(self.weights.view(-1))
 		mask_els = masks.size()[0]
 		zero_indicies = randomarraygenerator(sparsity, mask_els)
 		for zi in zero_indicies:
 			masks[zi] = 1
-		self.masks = torch.reshape(masks, self.weights.shape)
+		masks = torch.reshape(masks, self.weights.shape)
+		self.masks = nn.Parameter(masks)
+		self.masks.requires_grad = False
 
 	def numel(self):
 		return int(sum(mask.view(-1).size(0) for mask in self.masks))
